1e3e8451c75011ff0e4d4c0794fca656b8fadb51
unknown <wgfufg88@gmail.com>
2026-09-25 11:01:39 +0800

    commit first

---
+++ recipe/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
+fi
+
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:$BUILD_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# See https://github.com/abseil/abseil-cpp/releases/tag/20230125.0
+# We need to wait for upstream to use the new flags
+export CXXFLAGS="$CXXFLAGS -DABSL_LEGACY_THREAD_ANNOTATIONS=1"
+
+cmake ${CMAKE_ARGS} .. \
+      -G "Ninja" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_STANDARD=17 \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+  export CTEST_OUTPUT_ON_FAILURE=1
+  cmake --build . --config Release --target test
+fi
